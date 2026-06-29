1df566fc250918dfe4a848cf5084173b72c47c1f
MelinaCora <melinaccora97@gmail.com>
2026-06-29 12:58:57 -0300

    primera tab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { addIcons } from 'ionicons';
+
+import {
+  logoIonic,
+  rocketOutline,
+  layersOutline,
+  phonePortraitOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +18,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private router: Router) {
+
+    addIcons({
+      logoIonic,
+      rocketOutline,
+      layersOutline,
+      phonePortraitOutline
+    });
+
+  }
+
+  explorarComponentes() {
+
+    this.router.navigate(['/tabs/tab2']);
+
+  }
 
 }
